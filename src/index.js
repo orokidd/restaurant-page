f49a8b242e94c07318239b33f608278c86ac4f30
orokidd <gregomily@gmail.com>
2025-04-16 23:13:40 +0800

    add eventlistener to nav buttons

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import { loadHome } from "./home";
 
 (function app() {
     const content = document.querySelector('#content');
+    const navs = document.querySelectorAll('.nav');
     content.appendChild(loadHome());
+
+    navs.forEach((nav) => {
+        nav.addEventListener('click', () => handleNavClick(nav))
+    })
+
 })();
 
+function handleNavClick(nav) {
+    if (nav.className === "nav-home") {
+
+    }
+}
 
