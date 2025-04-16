6583755a19c27c46840584652d771aa3bfd2f076
orokidd <gregomily@gmail.com>
2025-04-16 21:00:03 +0800

    added className in home page

--- src/home.js
+++ src/home.js
@@ -1,23 +1,35 @@
 function createHeroSection() {
-    const home = document.createElement("div")
+    const hero = document.createElement("div")
     const heroText = document.createElement("h1")
 
+    hero.className = "hero";
+    heroText.className = "hero-text"
     heroText.textContent = "Orokidd arasaka restaurant"
 
-    home.appendChild(heroText);
+    hero.appendChild(heroText);
 
-    return home;
+    return hero;
 }
 
 function createSecondSection() {
     const section = document.createElement("div")
+    const textContent = document.createElement("div")
+    const imageContent = document.createElement("div")
     const headerText = document.createElement("h1")
     const paraText = document.createElement("p")
+
+    section.className = "second";
+    textContent.className = "text-content";
+    imageContent.className = "image-content";
+    headerText.className = "header";
+    paraText.className = "text";
+
 
     headerText.textContent = "A healthier way now has a seat at the table"
     paraText.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et mauris ligula. Vestibulum sed mi in odio tempus mollis ut vitae est."
 
-    section.append(headerText, paraText);
+    textContent.append(headerText, paraText)
+    section.append(textContent, imageContent);
 
     return section;
 }
@@ -27,6 +39,10 @@ function createMenuSection() {
     const headerText = document.createElement("h1")
     const menuList = document.createElement("ul")
     const menus = ['Ayam Bakar','Nasi Goreng','Gado-gado','Soto Makassar'];
+
+    section.className = "menu";
+    headerText.className = "header";
+    menuList.className = "menu-list";
 
     headerText.textContent = "Peak at our menu!"
     menus.forEach((menu) => {
@@ -45,6 +61,11 @@ function createBookingSection() {
     const headerText = document.createElement("h1")
     const paraText = document.createElement("p")
     const bookButton = document.createElement("button")
+
+    section.className = "booking";
+    headerText.className = "header";
+    paraText.className = "text";
+    bookButton.className = "book-button"
 
     headerText.textContent = "Don't Wait - Book Now!"
     paraText.textContent = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
